a136551fba15e600092cb8a2b3cfadb6728d1fbc
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-07-23 13:32:33 +0300

    tests done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,67 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static boolean isPalindrome(int num) {
+        num = Math.abs(num);
+        char[] digits = String.valueOf(num).toCharArray();
+
+        if(digits[0] != digits[digits.length-1]) return false;
+
+        int startIndex = 0;
+        int endIndex = digits.length - 1;
+
+        for(int i = 0; i < digits.length; i++) {
+
+            if(digits[startIndex] != digits[endIndex]) return false;
+            else {
+                startIndex++;
+                endIndex--;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPerfectNumber(int num) {
+        if(num < 0) return false;
+
+        int sum = 0;
+
+        for (int i = 1; i < num; i++) {
+            if(num % i == 0) sum += i;
+        }
+
+        return sum == num;
+    }
+
+    public static String numberToWords(int num) {
+        if(num < 0) return "Invalid Value";
+
+        String[] numNames = {
+                "Zero",
+                "One",
+                "Two",
+                "Three",
+                "Four",
+                "Five",
+                "Six",
+                "Seven",
+                "Eight",
+                "Nine"
+        };
+
+        char[] digits = String.valueOf(num).toCharArray();
+
+        String numsInWords = "";
+
+        for (int i = 0; i < digits.length; i++) {
+            if(i == digits.length-1) {
+                numsInWords += numNames[Integer.parseInt(String.valueOf(digits[i]))];
+            } else numsInWords += numNames[Integer.parseInt(String.valueOf(digits[i]))] + " ";
+        }
+
+        return numsInWords;
+
+    }
 }
